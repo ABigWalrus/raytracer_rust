45614482e179d3fcf7b758b308fa8b8d018174a5
BigWalrus <morgjik@gmail.com>
2025-11-07 23:56:52 +0100

    add renderer, state and app

---
+++ src/app.rs
@@ -0,0 +1,271 @@
+use std::sync::Arc;
+
+use wgpu::SurfaceTexture;
+use winit::{
+    application::ApplicationHandler, dpi::PhysicalSize, event::WindowEvent, window::Window,
+};
+
+#[derive(Default)]
+pub struct RayTracer {
+    window: Option<Window>,
+}
+
+struct State {
+    surface: wgpu::Surface<'static>,
+    window_size: PhysicalSize<u32>,
+    device: wgpu::Device,
+    compute_pipeline: wgpu::ComputePipeline,
+    compute_bind_group: wgpu::BindGroup,
+    render_pipeline: wgpu::RenderPipeline,
+    render_bind_group: wgpu::BindGroup,
+}
+
+impl State {
+    async fn new(window: Arc<Window>) -> Self {
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window.clone()).unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptionsBase {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                experimental_features: wgpu::ExperimentalFeatures::default(),
+                required_limits: wgpu::Limits::default(),
+                memory_hints: Default::default(),
+                trace: wgpu::Trace::Off,
+            })
+            .await
+            .unwrap();
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .find(|f| f.is_srgb())
+            .unwrap_or(surface_capabilities.formats[0]);
+
+        let window_size = window.inner_size();
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: window_size.width,
+            height: window_size.height,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        surface.configure(&device, &config);
+
+        // Texture that will be used to render results of compute shader
+        let render_texture = device.create_texture(&wgpu::wgt::TextureDescriptor {
+            label: Some("render_texture"),
+            size: wgpu::Extent3d {
+                width: window_size.width,
+                height: window_size.height,
+                depth_or_array_layers: 1,
+            },
+            mip_level_count: 1,
+            sample_count: 1, // TODO: here we can setup that the texture can be multi sampled
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8Unorm,
+            usage: wgpu::TextureUsages::STORAGE_BINDING | wgpu::TextureUsages::TEXTURE_BINDING,
+            view_formats: &[],
+        });
+
+        let texture_view = render_texture.create_view(&wgpu::wgt::TextureViewDescriptor::default());
+        let sampler = device.create_sampler(&wgpu::wgt::SamplerDescriptor::default());
+
+        let compute_shader =
+            device.create_shader_module(wgpu::include_wgsl!("../shaders/compute.wgsl"));
+
+        let compute_bind_group_layot =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("compute_bg_layout"),
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::StorageTexture {
+                        access: wgpu::StorageTextureAccess::WriteOnly,
+                        format: wgpu::TextureFormat::Rgba8Unorm,
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                    },
+                    count: None,
+                }],
+            });
+
+        let compute_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("compute_bg"),
+            layout: &compute_bind_group_layot,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&texture_view),
+            }],
+        });
+
+        let compute_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("compute_pipeline_layout"),
+                bind_group_layouts: &[&compute_bind_group_layot],
+                push_constant_ranges: &[],
+            });
+
+        let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: Some("compute_pipeline"),
+            layout: Some(&compute_pipeline_layout),
+            module: &compute_shader,
+            entry_point: Some("main"),
+            compilation_options: Default::default(),
+            cache: None,
+        });
+
+        let render_shader =
+            device.create_shader_module(wgpu::include_wgsl!("../shaders/render.wgsl"));
+
+        let render_bind_group_layot =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("render_bg_layout"),
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            multisampled: false,
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                ],
+            });
+
+        let render_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("render_bg"),
+            layout: &render_bind_group_layot,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&texture_view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&sampler),
+                },
+            ],
+        });
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("render_pipeline_layout"),
+                bind_group_layouts: &[&render_bind_group_layot],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("render_pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &render_shader,
+                entry_point: Some("vs_main"),
+                compilation_options: Default::default(),
+                buffers: &[],
+            },
+            primitive: wgpu::PrimitiveState::default(),
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState::default(),
+            fragment: Some(wgpu::FragmentState {
+                module: &render_shader,
+                entry_point: Some("fs_main"),
+                compilation_options: Default::default(),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: surface_format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            multiview: None,
+            cache: None,
+        });
+
+        Self {
+            surface,
+            window_size,
+            device,
+            compute_pipeline,
+            compute_bind_group,
+            render_pipeline,
+            render_bind_group,
+        }
+    }
+
+    fn render(&self) {
+        let frame = self.surface.get_current_texture().unwrap();
+        let view = frame.texture.create_view(&Default::default());
+
+        let mut encoder =
+            self.device
+                .create_command_encoder(&wgpu::wgt::CommandEncoderDescriptor {
+                    label: Some("render_encoder"),
+                });
+
+        let mut pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+            label: Some("compute_pass"),
+            timestamp_writes: None,
+        });
+
+        pass.set_pipeline(&self.compute_pipeline);
+        pass.set_bind_group(0, &self.compute_bind_group, &[]);
+        pass.dispatch_workgroups(self.window_size.width, self.window_size.height, 1);
+    }
+}
+
+impl ApplicationHandler for RayTracer {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
 use std::{fs::File, time::Instant};
 
+use winit::event_loop::{ControlFlow, EventLoop};
+
 use crate::{
+    app::RayTracer,
     math::Vec3,
     rt_core::{Camera, Color, Dielectric, HittableList, Lambertian, Metal, Sphere},
 };
 
+mod app;
 mod math;
 mod rt_core;
 mod util;
 
 fn main() {
-    println!("=================");
-    println!("Starting ray tracing");
-    println!("=================");
+    // println!("=================");
+    // println!("Starting ray tracing");
+    // println!("=================");
 
-    let file = File::create("image.ppm").unwrap();
+    // let file = File::create("image.ppm").unwrap();
 
-    let mut world = HittableList::new();
+    // let mut world = HittableList::new();
 
-    let material_ground = Lambertian::new(Color::new(0.5, 0.5, 0.5));
+    // let material_ground = Lambertian::new(Color::new(0.5, 0.5, 0.5));
 
-    world.add(Sphere::new(
-        Vec3::new(0.0, -1000.0, 0.0),
-        1000.0,
-        material_ground,
-    ));
+    // world.add(Sphere::new(
+    //     Vec3::new(0.0, -1000.0, 0.0),
+    //     1000.0,
+    //     material_ground,
+    // ));
 
     // for a in -11..11 {
     //     for b in -11..11 {
@@ -53,29 +57,36 @@ fn main() {
     //     }
     // }
 
-    let sphere_material = Dielectric::new(1.5);
-    world.add(Sphere::new(Vec3::new(0.0, 1.0, 0.0), 1.0, sphere_material));
+    // let sphere_material = Dielectric::new(1.5);
+    // world.add(Sphere::new(Vec3::new(0.0, 1.0, 0.0), 1.0, sphere_material));
 
-    let sphere_material = Lambertian::new(Color::new(0.4, 0.2, 0.1));
-    world.add(Sphere::new(Vec3::new(-4.0, 1.0, 0.0), 1.0, sphere_material));
+    // let sphere_material = Lambertian::new(Color::new(0.4, 0.2, 0.1));
+    // world.add(Sphere::new(Vec3::new(-4.0, 1.0, 0.0), 1.0, sphere_material));
 
-    let sphere_material = Metal::new(Color::new(0.7, 0.6, 0.5), 0.0);
-    world.add(Sphere::new(Vec3::new(4.0, 1.0, 0.0), 1.0, sphere_material));
+    // let sphere_material = Metal::new(Color::new(0.7, 0.6, 0.5), 0.0);
+    // world.add(Sphere::new(Vec3::new(4.0, 1.0, 0.0), 1.0, sphere_material));
 
-    let camera = Camera::build(
-        16.0 / 9.0,
-        // 1200,
-        300,
-        20.0,
-        Vec3::new(13.0, 2.0, 3.0),
-        Vec3::new(0.0, 0.0, 0.0),
-        10,
-        50,
-        0.6,
-        10.0,
-    );
-    let now = Instant::now();
-    camera.render(&world, file);
+    // let camera = Camera::build(
+    //     16.0 / 9.0,
+    //     // 1200,
+    //     300,
+    //     20.0,
+    //     Vec3::new(13.0, 2.0, 3.0),
+    //     Vec3::new(0.0, 0.0, 0.0),
+    //     10,
+    //     50,
+    //     0.6,
+    //     10.0,
+    // );
+    // let now = Instant::now();
+    // camera.render(&world, file);
 
-    println!("\nDone, it took: {:?}", now.elapsed());
+    // println!("\nDone, it took: {:?}", now.elapsed());
+
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut ray_tracer = RayTracer::default();
+
+    event_loop.run_app(&mut ray_tracer);
 }
